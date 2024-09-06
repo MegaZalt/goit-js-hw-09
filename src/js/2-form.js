69a3665034abce0256f4b3b66913953aec450c9b
Valeriia <malvinacat0404@gmail.com>
2024-09-06 16:35:29 +0200

    11

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,24 @@
-console.log("Form");
+function saveForm() {
+    const formData = {
+        email: document.querySelector('input[name="email"]').value,
+        message: document.querySelector('textarea[name="message"]').value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+function loadForm() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        document.querySelector('input[name="email"]').value = email || '';
+        document.querySelector('textarea[name="message"]').value = message || '';
+    }
+
+    document.querySelector('.feedback-form').addEventListener('input', saveForm);
+    window.addEventListener('load', loadForm);
+    document.querySelector('feedback-form').addEventListener('submit', () => {
+        localStorage.removeItem('feedback-form-state');
+    })
+};
