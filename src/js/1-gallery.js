f6598f1beb3d01289efee7e6238e9a8d29ef7a41
Valeriia <malvinacat0404@gmail.com>
2024-09-06 14:50:24 +0200

    11

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,69 +1,83 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { preview } from 'vite';
-
 const galleryItems = [
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 1 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 2 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 3 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 4 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 5 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 6 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 7 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 8 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
 
-    {
-        preview: 'small-image.jpg',
-        original: 'large-image.jpg',
-        description: 'Image 9 description',
-    },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
 ];
 
 const galleryContainer = document.querySelector('.gallery');
 
-const galleryGalleryItem = (items) => {
-    return items;
+const galleryGalleryItem = items => {
+  return items
     .map(({ preview, original, description }) => {
-        return `
+      return `
         <li class="gallery-item">
           <a class="gallery-link" href="${original}">
             <img
@@ -77,3 +91,14 @@ const galleryGalleryItem = (items) => {
     })
     .join('');
 };
+
+galleryContainer.innerHTML = galleryGalleryItem(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  enableKeyboard: true,
+});
+
+console.log("SimpleLightbox initialized")
